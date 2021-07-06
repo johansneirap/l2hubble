fd84115ce163b925eed379e7a9200b31a829ba81
Johans <johansneirap@gmail.com>
2021-07-06 18:26:06 -0400

    add paypal test client JS

--- scripts/userPanel.js
+++ scripts/userPanel.js
@@ -647,29 +647,4 @@ const populateHistoryDonation = (donations)=>{
     }
 }
 
-//stripeeeeeeeee
-// Create an instance of the Stripe object with your publishable API key
-var stripe = Stripe("pk_test_51J5y6GLGRoTXAwQE2ptAVXEYrzQXHxl0IkkVeQn0cgLbfyor5S7jxSrEON1BEFFAlqNWtRdeIXs6SWwSiXXmWniU00NJ5tuhDZ");
-var checkoutButton = document.getElementById("checkout-button");
-checkoutButton.addEventListener("click", function () {
-  fetch("http://localhost:4242/create-checkout-session", {
-    method: "POST",
-  })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (session) {
-      return stripe.redirectToCheckout({ sessionId: session.id });
-    })
-    .then(function (result) {
-      // If redirectToCheckout fails due to a browser or network
-      // error, you should display the localized error message to your
-      // customer using error.message.
-      if (result.error) {
-        alert(result.error.message);
-      }
-    })
-    .catch(function (error) {
-      console.error("Error:", error);
-    });
-});
+//paypal
